21d636a448f7a8d59baec20654a3939c776f4963
yinnj <yinnj@2345.com>
2020-11-04 11:33:07 +0000

    modify

--- src/main/java/com/game/MainApplication.java
+++ src/main/java/com/game/MainApplication.java
@@ -1,9 +1,7 @@
 package com.game;
 
-import com.game.controller.IndexController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class MainApplication {
